60c63eca4bcb26c80e2b7618bce5a2a47e21d605
Kai Chan <owenkaic@gmail.com>
2022-09-29 15:07:25 +0100

    first commit

---
+++ src/Nav.js
@@ -0,0 +1,23 @@
+
+const Nav = () => {
+
+<header>
+        <nav>
+            <ul>
+                <li>
+                    <a href="#H1" target="_blank">Home</a>
+                </li>
+                <li>
+                    <a href="#products" target="_blank">Products</a>
+                </li>
+                
+                <li>
+                    <a href="section-footer" target="_blank"> Contact </a>
+                </li>
+            </ul>
+        </nav>
+  </header>
+
+}
+
+export default Nav;
---
+++ src/Title.js
@@ -0,0 +1,8 @@
+
+const Title = () => {
+
+<title> Cakeeeeee</title>
+
+}; 
+
+export default Title;
